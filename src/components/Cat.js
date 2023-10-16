987363b843b2ef91fe556a37c4aa1982620f8ed4
save_ole <xgz.cz@foxmail.com>
2023-10-16 23:51:07 +0800

    React Suspense lazy : dynamic loading example

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+
+const Cat = () => {
+  return (
+    <Avatar src="https://s1-imfile.feishucdn.com/static-resource/v1/v2_66675e6d-64d3-48f1-962c-c95113de228g~?image_size=72x72&cut_type=&quality=&format=png&sticker_format=.webp" />
+  );
+};
+
+export default Cat;
