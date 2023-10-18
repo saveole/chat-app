7d5c1dd8404668515dcbf2aeb66597bcc9568b18
save_ole <xgz.cz@foxmail.com>
2023-10-18 22:13:31 +0800

    search bar style not working

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -1,25 +1,90 @@
-import { Box, IconButton, Typography } from "@mui/material";
-import { CircleDashed, Stack } from "phosphor-react";
+import {
+  Box,
+  IconButton,
+  Typography,
+  Stack,
+  InputBase,
+  Button,
+  Divider,
+} from "@mui/material";
+import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
 import React from "react";
+import { styled, alpha } from "@mui/material/styles";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: 20,
+  backgroundColor: alpha(theme.palette.background.paper, 1),
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "&. MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from seachIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: "100%",
+  },
+}));
 
 const Chats = () => {
   return (
     <Box
       sx={{
         position: "relative",
-        height: "100%",
+        height: "100vh",
         width: 320,
         backgroundColor: "#F8FAFF",
         boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
       }}
     >
-      <Stack>
-        <Typography variant="h5">Chats</Typography>
-      </Stack>
+      <Stack p={3} spacing={2}>
+        <Stack
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+        >
+          <Typography variant="h5">Chats</Typography>
+          <IconButton>
+            <CircleDashed />
+          </IconButton>
+        </Stack>
 
-      <IconButton>
-        <CircleDashed />
-      </IconButton>
+        {/** Search bar */}
+        <Stack sx={{ width: "100%" }}>
+          <Search>
+            <SearchIconWrapper>
+              <MagnifyingGlass color="#709CE6" />
+              <StyledInputBase
+                placeholder="Search..."
+                inputProps={{ "aria-label": "search" }}
+              />
+            </SearchIconWrapper>
+          </Search>
+        </Stack>
+
+        {/** Archive */}
+        <Stack spacing={1}>
+          <Stack direction={"row"} alignItems={"center"} spacing={1.5}>
+            <ArchiveBox size={24} />
+            <Button>Archive</Button>
+          </Stack>
+          <Divider />
+        </Stack>
+      </Stack>
     </Box>
   );
 };
